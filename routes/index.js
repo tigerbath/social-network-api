b07924530806801361c8b02f4a4b728b47c18777
Matthew Palmer <mattjpalmer92@gmail.com>
2022-03-25 15:49:55 +0000

    controller fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-const apiRoutes = require("./api");
+const api = require("./api");
 
 // add prefix of `/api` to all of the api routes imported from the `api` directory
-router.use("/api", apiRoutes);
+router.use("/api", apis);
 
 router.use((req, res) => {
   res.status(404).send("<h1>404 Error!</h1>");
