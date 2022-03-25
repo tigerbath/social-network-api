fbd7511b9c67369b7da8025f980f17fd3cb6e825
Matthew Palmer <mattjpalmer92@gmail.com>
2022-03-25 16:31:49 +0000

    user by id

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -14,6 +14,21 @@ const getUsers = async (req, res) => {
   }
 };
 
+// get user by id
+
+const getUserById = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    return res.json({ success: true, data: user });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get User | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to get User" });
+  }
+};
+
 // create user
 
 // update user
@@ -22,4 +37,5 @@ const getUsers = async (req, res) => {
 
 module.exports = {
   getUsers,
+  getUserById,
 };
