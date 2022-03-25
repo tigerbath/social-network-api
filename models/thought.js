c81c838e6d7985169d4dfdcc6af2af96f6b472b1
Matthew Palmer <mattjpalmer92@gmail.com>
2022-03-25 16:06:17 +0000

    schema params

--- models/thought.js
+++ models/thought.js
@@ -1,63 +1,38 @@
-const { Schema, model, Types } = require("mongoose");
-const moment = require("moment");
+const { Schema, model } = require("mongoose");
+const { formatTimestamp } = require("../utils");
 
-const ReplySchema = new Schema({
-  // set custom id to avoid confusion with parent comment _id
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1,
-    maxlength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) =>
-      moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
-  },
-});
+const reactionSchema = require("./Reaction");
 
-const ThoughtSchema = new Schema({
+const thoughtSchema = {
   thoughtText: {
     type: String,
     required: true,
-    minlength: 1,
-    maxlength: 280,
+    minLength: 1,
+    maxLength: 100,
   },
   createdAt: {
     type: Date,
-    default: Date.now,
-    get: (createdAtVal) =>
-      moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    default: Date.now(),
+    get: formatTimestamp,
   },
   username: {
     type: String,
     required: true,
   },
-  reactions: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "reactionSchema",
-    },
-  ],
+  reactions: [reactionSchema],
+};
+
+const schema = new Schema(thoughtSchema, {
+  toJSON: {
+    getters: true,
+  },
+  id: false,
 });
 
-// get total count of friends on retrieval
-ThoughtSchema.virtual("reactionCount").get(function () {
-  return this.reactions.reduce(
-    (total, reaction) => total + reaction.count.length + 1,
-    0
-  );
+schema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
-const Thought = model("Thought", ThoughtSchema);
+
+const Thought = model("thought", schema);
 
 module.exports = Thought;
